4fa0c69ab88f850eda3f855f3067e6e621fcd350
sebpost2 <81270882+sebpost2@users.noreply.github.com>
2024-07-03 18:16:52 -0500

    Update consumerStream.py

--- consumerStream.py
+++ consumerStream.py
@@ -4,61 +4,51 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, count, lit, sum as _sum
 
 conf = {
-    'bootstrap.servers': 'Slave2v2:9092',
+    'bootstrap.servers': 'Slave1v2:9092',
     'group.id': 'python-consumer',
-    'auto.offset.reset': 'earliest',
-    'enable.auto.commit': False  # Disable auto commit for batch processing
+    'auto.offset.reset': 'earliest'
 }
 
 spark = SparkSession.builder \
-    .appName("KafkaConsumerWithSparkBatch") \
+    .appName("KafkaConsumerWithSpark") \
     .getOrCreate()
 
-schema = "user_location STRING, total_count LONG"
-total_user_locations = spark.createDataFrame([], schema)
+schema = "user_location STRING, genre STRING, title STRING, watchfrequency LONG"
+total_data = spark.createDataFrame([], schema)
 
-def consume_and_process_with_spark_batch():
-    global total_user_locations
+def consume_and_process_with_spark():
+    global total_data
     consumer = Consumer(conf)
 
     try:
-        consumer.subscribe(['Proyecto'])  # Subscribe to Kafka topic 'Proyecto'
+        consumer.subscribe(['Proyecto'])
 
         while True:
-            messages = consumer.consume(num_messages=10, timeout=10.0)
-
-            if not messages:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None:
                 continue
+            if msg.error():
+                if msg.error().code() == KafkaException._PARTITION_EOF:
+                    continue
+                else:
+                    print(msg.error())
+                    break
 
-            batch_data = []
-            for msg in messages:
-                if msg.error():
-                    if msg.error().code() == KafkaException._PARTITION_EOF:
-                        continue
-                    else:
-                        print(msg.error())
-                        break
+            value = msg.value().decode('utf-8')
+            netflix_data = json.loads(value)
 
-                value = msg.value().decode('utf-8')
-                netflix_data = json.loads(value)
-                batch_data.append(netflix_data)
+            df = spark.createDataFrame([netflix_data])
 
-            df = spark.createDataFrame(batch_data)
-            current_location_counts = df.groupBy(
-                'user_location').agg(count('*').alias('count'))
+            df = df.withColumn('watch_count', lit(1))
 
-            current_location_counts = current_location_counts.withColumnRenamed(
-                'count', 'total_count')
+            current_data_counts = df.groupBy('user_location', 'genre', 'title') \
+                                    .agg(_sum(col('watch_count')).alias('watchfrequency'))
 
-            total_user_locations = total_user_locations.union(current_location_counts) \
-                .groupBy('user_location').agg(_sum(col('total_count')).alias('total_count'))
+            total_data = total_data.union(current_data_counts) \
+                .groupBy('user_location', 'genre', 'title').agg(_sum(col('watchfrequency')).alias('watchfrequency'))
 
-            total_user_locations.show()
-
-            consumer.commit()
-            total_user_locations.write.mode('overwrite').csv(
-                "hdfs://Masterv2:9000/spark")
-
+            total_data.show()
+            total_data.write.mode('overwrite').csv("hdfs://Masterv2:9000/sparkv2/Slave1v2")
     except KeyboardInterrupt:
         pass
 
@@ -66,7 +56,5 @@ def consume_and_process_with_spark_batch():
         consumer.close()
         spark.stop()
 
-
-# Call the consumer function
 if __name__ == '__main__':
-    consume_and_process_with_spark_batch()
+    consume_and_process_with_spark()
